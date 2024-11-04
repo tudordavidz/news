51f282729ee5418bbf7ae78b0974f24b1cb048d8
tudordavidz <dvd_tudor@yahoo.com>
2024-11-04 17:07:57 +0200

    add search bar

---
+++ components/Searchbar.tsx
@@ -0,0 +1,48 @@
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "@/constants/Colors";
+type Props = {};
+
+const SearchBar = (props: Props) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchBar}>
+        <Ionicons name="search-outline" size={24} color={Colors.lightGrey} />
+        <TextInput
+          placeholder="Search"
+          placeholderTextColor={Colors.lightGrey}
+          style={styles.searchTxt}
+          autoCapitalize="none"
+        />
+      </View>
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+  },
+  searchBar: {
+    backgroundColor: "#E4E4E4",
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+    borderRadius: 10,
+    flexDirection: "row",
+    gap: 10,
+  },
+  searchTxt: {
+    fontSize: 16,
+    color: Colors.darkGrey,
+    flex: 1,
+  },
+});
